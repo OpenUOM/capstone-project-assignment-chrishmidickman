f0317d060192b33622feaab682b4bad0758829a6
chrishmidickman <118650052+chrishmidickman@users.noreply.github.com>
2022-11-21 12:32:55 +0530

    Update students.js

--- backend/seeds/students.js
+++ backend/seeds/students.js
@@ -1,7 +1,9 @@
 exports.seed = function(knex) {
   return knex('student').del()
     .then(function () {
-      return knex('student').insert([
+      return knex('student').insert([{"age": 10, "hometown": "Colombo", "id": 20001, "name": "Supun Mihiranga"},
+                                     {"age": 9, "hometown": "Galle", "id": 20002, "name": "Sandun Perera"},
+                                     {"age": 10, "hometown": "Kandy", "id": 20003, "name": "Isuri De Silva"}
         
       ]);
     });
